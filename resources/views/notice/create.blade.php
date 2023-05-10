35fc88b2c3b4ab7cf98b92e9ced3951abd72d1db
Govinda Parajuli <parajuligovinda541@gmail.com>
2023-05-10 11:00:34 +0545

    4thcommit

--- resources/views/notice/create.blade.php
+++ resources/views/notice/create.blade.php
@@ -7,17 +7,17 @@
 
     <form action="{{ route('notice.store')}}" method="POST" class="mt-5">
 @csrf
-        <input type="text" placeholder="Name" name="name" class="w-full rounded-lg border-gray-300 my-2" value="{{old('name')}}">
+        <input type="text" placeholder="Notice" name="name" class="w-full rounded-lg border-gray-300 my-2" value="{{old('notice')}}">
         @error('name')
             <p class="text-red-600 text-xs -mt-2">{{$message}}</p>
         @enderror
-        <input type="text" placeholder="Notice" name="priority" class="w-full rounded-lg border-gray-300 my-2"value="{{old('priority')}}">
+        <input type="text" placeholder="Priority" name="priority" class="w-full rounded-lg border-gray-300 my-2"value="{{old('priority')}}">
         @error('priority')
         <p class="text-red-600 text-xs -mt-2">{{$message}}</p>
     @enderror
         <div class="flex">
     <input class="bg-amber-400 text-black px-4 py-2 rounded-lg shadow-md hover:shadow-amber-300" type="submit" value="Add Notice">
-    <a class="bg-red-600 text-black px-4 py-2 mx-2 rounded-lg shadow-md hover:shadow-amber-300"href="{{route('category.index')}}">Exit</a>
+    <a class="bg-red-600 text-black px-4 py-2 mx-2 rounded-lg shadow-md hover:shadow-amber-300"href="{{route('notice.index')}}">Exit</a>
 
 
 
